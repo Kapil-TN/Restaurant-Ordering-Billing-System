c1fceb8fcf1e2e3196c3acae994225ddf709f91d
Developer <developer@example.com>
2026-06-17 19:01:43 +0530

    Setup Flask app factory and database

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,1 +1,18 @@
-# Initialize app package
+from flask import Flask
+from .extensions import db, migrate
+
+def create_app():
+
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = \
+        "sqlite:///restaurant.db"
+
+    app.config["SECRET_KEY"] = "dev"
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .models.user import User
+
+    return app
